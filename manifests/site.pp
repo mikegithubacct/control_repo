f77f44a07b1cc88a74ca5ca9fd9d380bfbd2eec8
mikegithubacct <65126241+mikegithubacct@users.noreply.github.com>
2020-05-10 19:03:58 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node default {
   file {'/root/README':
-    ensure => file,
-    content => 'This is a readme file!'
+    ensure  => file,
+    content => 'This is a readme file!',
+    owner   => 'root',
   }
 }
