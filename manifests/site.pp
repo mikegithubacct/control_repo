57710d378a46cbbc0102b72063ce80c7fc060dd6
mikegithubacct <65126241+mikegithubacct@users.noreply.github.com>
2020-05-15 16:26:59 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,21 @@
 node default {
-}
-node 'master.puppet.vm' {
-  include role::master_server
   file { '/root/README':
     ensure => file,
-    content => "Welcome to ${fqdn}\n",
+    content => 'This is a readme',
     owner   => 'root',
   }
 }
-
-node /^web/ {
+node 'master.puppet.vm' {
+  include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
+  }
+}
+node /^web/ { 
   include role::app_server
 }
-
 node /^db/ {
   include role::db_server
 }
-  
